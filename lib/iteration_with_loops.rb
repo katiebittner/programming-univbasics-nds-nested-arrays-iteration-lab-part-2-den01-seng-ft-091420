bbba145807d8a4afc3a8141fbf0dcddb50bf3c4c
Katie B <katiebittner1@gmail.com>
2020-08-30 18:50:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
   
   row_index = 0 
   while row_index < src.count do
-    src[row_index].sort.
+    smallest_numbers >> src[row_index].sort.shift
     end
     row_index += 1
   end
